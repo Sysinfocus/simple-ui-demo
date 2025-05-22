4d4965059d611f91ffe9cc7ff16dd8f910b595c3
Sysinfocus <109056087+Sysinfocus@users.noreply.github.com>
2025-05-22 09:39:51 +0000

    Deploying to gh-pages from @ Sysinfocus/simple-ui-demo@910d5a11d3b5116653b4961a653fda0a13eade27 🚀

---
+++ _content/Sysinfocus.AspNetCore.Components/ServerSentEvents.razor.js
@@ -0,0 +1,16 @@
+﻿let started = false;
+let eventSource;
+
+export const init = (url, dno) => {
+    started = true;
+    eventSource = new EventSource(url);
+    if (!started) return;
+    eventSource.onopen = e => dno?.invokeMethodAsync('HandleOnOpen', "The connection has been established.");
+    eventSource.onmessage = e => dno?.invokeMethodAsync('HandleOnMessage', e.data, e.lastEventId, e.origin, e.ports, e.source);
+    eventSource.onerror = e => dno?.invokeMethodAsync('HandleOnError', "An error occurred while attempting to connect.");    
+}
+
+export const stop = () => {
+    started = false;
+    eventSource.close();
+}
